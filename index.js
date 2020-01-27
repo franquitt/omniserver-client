b35edc9f7629f2bc434c35915eb4084b3577a74c
Franco Agresta Gonzalez <franquitt@gmail.com>
2020-01-27 08:47:45 -0300

    first init

---
+++ index.js
@@ -0,0 +1,55 @@
+var socket = require('socket.io-client')('http://localhost:1335');
+
+//funcion que se ejecuta al conectarse
+socket.on('connect', () => {
+
+	console.log("Enlace conectado");
+
+	socket.emit("sesion", {
+		action: "login",
+		user: "demo3",
+		pass: "admin"
+	})
+
+});
+
+
+//funcion que procesa los mensajes provenientes del topic sesion
+socket.on('sesion', (msg) => {
+	console.log("sesion");
+	console.log(msg);
+	//nos fijamos de que verbo se trataba
+	switch(msg.action){
+		case "login":
+			if(msg.response == "success"){
+				socket.emit("chats", {
+					action: "getHome"
+				});
+			}
+		break;
+	}
+});
+
+//funcion que procesa los mensajes provenientes del topic chats
+socket.on('chats', (msg) => {
+
+	console.log("chats");
+	switch(msg.action){
+		case "getHome":
+			console.log("Se muestran los primeros 2 msg del home");
+			console.log(msg.payload.slice(0, 2));
+			socket.emit("sesion", {
+					action: "timeout"
+				});
+		break;
+	}
+
+});
+
+
+//funcion que se ejecuta al desconectarse
+socket.on('disconnect', data => {
+
+	console.log("Enlace desconectado");
+
+});
