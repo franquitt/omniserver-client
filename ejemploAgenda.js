38d5f88acd8a4f1faa6caaedc4302a6d35db305b
Franco Agresta Gonzalez <franquitt@gmail.com>
2020-11-25 16:29:27 -0300

    agregado ejemplo de agenda

---
+++ ejemploAgenda.js
@@ -0,0 +1,74 @@
+var socket = require('socket.io-client')('http://localhost:1335');
+
+//funcion que se ejecuta al conectarse
+socket.on('connect', () => {
+
+	console.log("Enlace conectado");
+
+	socket.emit("sesion", {
+		action: "login",
+		user: "USUARIO",
+		pass: "PASSWORD"
+	})
+
+});
+
+
+//funcion que procesa los mensajes provenientes del topic sesion
+socket.on('sesion', (msg) => {
+	console.log("sesion");
+	console.log(msg);
+	//nos fijamos de que verbo se trataba
+	switch (msg.action) {
+		case "login":
+			if (msg.response == "success") {
+				/*socket.emit("agenda", {
+					action: "getTypes"
+				});*/
+
+				socket.emit("agenda", {
+					action: 'updateType', payload: {
+						activo: true,
+						borrado: false,
+						id: 23,
+						listar: true,
+						nombre: 'test',
+						obligatorio: false,
+						orden: 29,
+						placeholder: '',
+						primario: false,
+						valorConfigAgendaObject: [
+							{id: 852, valor: "opcionb"},
+							{id: 0, valor: "nuevoo test juan!"},
+
+						]
+					}
+				});
+
+			}
+			break;
+	}
+});
+
+//funcion que procesa los mensajes provenientes del topic chats
+socket.on('agenda', (msg) => {
+    console.log("agenda");
+
+    if(msg.tipos){
+        const tipo_select_test = msg.tipos.find(tipo => (tipo.id === 23))
+
+        console.log(tipo_select_test)
+    }else{
+        console.log(msg)
+    }
+
+
+});
+
+
+//funcion que se ejecuta al desconectarse
+socket.on('disconnect', data => {
+
+	console.log("Enlace desconectado");
+
+});
